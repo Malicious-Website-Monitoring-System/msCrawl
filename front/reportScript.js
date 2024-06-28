f1ef023c56bc10b9587f6e50084c274bcd898f71
Sun <132317759+mskim8033@users.noreply.github.com>
2024-06-28 13:00:30 +0900

    Add files via upload

---
+++ front/reportScript.js
@@ -0,0 +1,5 @@
+document.getElementById('report-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("신고가 접수되었습니다. 감사합니다!");
+    // 여기서 폼 데이터를 처리하고 서버로 전송하는 코드를 추가할 수 있습니다.
+});
